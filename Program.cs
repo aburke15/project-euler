018e0ff3d4400e2fc9c9f976907b209a6c3bcdad
Andre Burke <aburke.dev@gmail.com>
2021-09-22 23:45:14 -0600

    added mostly finished euler 8 problem

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var largestAdjacentProduct = new LargestAdjacentProductInSeries();
+
+System.Console.WriteLine(
+    largestAdjacentProduct.GetLargestAdjacentProduct(13)
+);
+
